2caf1a09da1fa3c4c13b73240ef2bbc9224750e8
Jon Dromey <jonathan.dromey@gmail.com>
2025-01-31 09:15:23 -0700

    Changed blinking code to have a larger delay

--- Src/lab1.c
+++ Src/lab1.c
@@ -15,7 +15,7 @@ int lab1_main(void) {
     HAL_GPIO_Init(GPIOC, &initStr);
     HAL_GPIO_WritePin(GPIOC, GPIO_PIN_8, GPIO_PIN_SET);
     while (1) {
-        HAL_Delay(200);
+        HAL_Delay(1000);
         HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_8 | GPIO_PIN_9);
     }
 
