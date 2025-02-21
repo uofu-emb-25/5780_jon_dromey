4440935c08f8706d6314aef356e64351ebb70506
Jon Dromey <jonathan.dromey@gmail.com>
2025-02-20 22:26:48 -0700

    Got all logic moved into helper functions and tested various values for CCR in both PWM modes

--- Inc/main.h
+++ Inc/main.h
@@ -4,8 +4,9 @@
 void SystemClock_Config(void);
 void UserButton_Interrupt_Init(void);
 void TIMER2_SETUP(void);
-void TIMER3_SETUP(void);
-void TIMER3_SETCCR(uint32_t ch1_val, uint32_t ch2_val);
+void TIMER3_SETUP(uint32_t initial_CCR);
+void TIMER3_SETCCR1(uint32_t ch1_val);
+void TIMER3_SETCCR2(uint32_t ch2_val);
 int lab1_main(void);
 int lab2_main(void);
 int lab3_main(void);
