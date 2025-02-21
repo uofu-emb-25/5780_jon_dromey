8fb00f4e3a550e20d10766294e45f644f3895832
Jon Dromey <jonathan.dromey@gmail.com>
2025-02-20 21:57:24 -0700

    Moved the code for part one into a function

--- Inc/main.h
+++ Inc/main.h
@@ -4,6 +4,8 @@
 void SystemClock_Config(void);
 void UserButton_Interrupt_Init(void);
 void TIMER2_SETUP(void);
+void TIMER3_SETUP(void);
+void TIMER3_SETCCR(uint32_t ch1_val, uint32_t ch2_val);
 int lab1_main(void);
 int lab2_main(void);
 int lab3_main(void);
