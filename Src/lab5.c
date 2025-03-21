dd7f21cc36b1dce379ffe470c264baf51b5a11eb
Jon Dromey <jonathan.dromey@gmail.com>
2025-03-20 23:28:47 -0600

    Got all lab5 code working

--- Src/lab5.c
+++ Src/lab5.c
@@ -1,6 +1,125 @@
 #include <stm32f0xx_hal.h>
 #include <assert.h>
 
+void writeRegister(uint32_t device, uint32_t reg, uint8_t toSend)
+{
+    /* Clear the NBYTES and SADD bit fields
+     * The NBYTES field begins at bit 16, the SADD at bit 0
+     */
+    I2C2->CR2 &= ~((0x7F << 16) | (0x3FF << 0));
+    // Set NBYTES and SADD
+    I2C2->CR2 |= (2 << 16) | (device << 1);
+
+    // Set the read bit low
+    I2C2->CR2 &= ~(0b01 << 10);
+
+    // Set start bit
+    I2C2->CR2 |= (0b01 << 13);
+
+    // Wait for either NACKF or TXIS
+    while (!(I2C2->ISR & 0b10010))
+        ;
+
+    if (I2C2->ISR & 0b10000) // NACKF
+    {
+        // Send stop bit
+        I2C2->CR2 |= (0b01 << 14);
+        My_HAL_GPIO_WritePin(GPIOC, 6, 1); // Turn on red LED for NACKF
+    }
+    else if (I2C2->ISR & 0b10) // TXIS
+    {
+        // Write the register address into the transmit data regiester
+        I2C2->TXDR = reg;
+
+        // Wait for either NACKF or TXIS
+        while (!(I2C2->ISR & 0b10010))
+            ;
+
+        if (I2C2->ISR & 0b10000) // NACKF
+        {
+            // Send stop bit
+            I2C2->CR2 |= (0b01 << 14);
+            My_HAL_GPIO_WritePin(GPIOC, 6, 1); // Turn on red LED for NACKF
+        }
+        else if (I2C2->ISR & 0b10) // TXIS
+        {
+            // Write the register content into the transmit data regiester
+            I2C2->TXDR = toSend;
+
+            // Wait for TC bit
+            while (!(I2C2->ISR & (1 << 6)))
+                ;
+
+            // Send stop bit
+            I2C2->CR2 |= (0b01 << 14);
+        }
+    }
+}
+
+uint32_t readXYData(uint32_t device)
+{
+    uint8_t sensorValues[4];
+    /* Clear the NBYTES and SADD bit fields
+     * The NBYTES field begins at bit 16, the SADD at bit 0
+     */
+    I2C2->CR2 &= ~((0x7F << 16) | (0x3FF << 0));
+    // Set SADD bits to 0x69 and NBYTES to 1
+    I2C2->CR2 |= (1 << 16) | (device << 1);
+
+    // Set the read bit low
+    I2C2->CR2 &= ~(0b01 << 10);
+
+    // Set start bit
+    I2C2->CR2 |= (0b01 << 13);
+
+    // Wait for either NACKF or TXIS
+    while (!(I2C2->ISR & 0b10010))
+        ;
+
+    if (I2C2->ISR & 0b10000) // NACKF
+    {
+        // Send stop bit
+        I2C2->CR2 |= (0b01 << 14);
+    }
+    else if (I2C2->ISR & 0b10) // TXIS
+    {
+        // Write the address to read into the transmit data regiester
+        I2C2->TXDR = 0xA8;
+
+        // Wait for TC bit
+        while (!(I2C2->ISR & (1 << 6)))
+            ;
+
+        // Set SADD bits to 0x69 and NBYTES to 1
+        I2C2->CR2 &= ~((0x7F << 16) | (0x3FF << 0));
+        I2C2->CR2 |= (4 << 16) | (device << 1);
+        // Set the read bit high
+        I2C2->CR2 |= (0b01 << 10);
+        // Set the start bit
+        I2C2->CR2 |= (0b01 << 13);
+
+        for (uint32_t i = 0; i < 4; i++)
+        {
+            // Wait for either NACKF or RXNE
+            while (!(I2C2->ISR & 0b10100))
+                ;
+            sensorValues[i] = I2C2->RXDR;
+        }
+
+        while (!(I2C2->ISR & 1 << 6))
+            ; // Wait for TC bit
+
+        // Send stop bit
+        I2C2->CR2 |= (0b01 << 14);
+    }
+    uint32_t returnValue = 0;
+    for (int i = 0; i < 4; i++) 
+    {
+        returnValue |= (sensorValues[i] << 8*i);
+    }
+    return returnValue;
+}
+
 int lab5_main()
 {
     HAL_Init();
@@ -47,88 +166,42 @@ int lab5_main()
     // Enable I2C
     I2C2->CR1 |= 0b01;
 
-    while (1)
-    {
-        HAL_Delay(1000);
+    // Write the needed info into CR1 of the gyroscope
+
+    writeRegister(0x69, 0x20, 0b00001011);
+
+    while(1) {
+        HAL_Delay(100);
+        uint32_t sensorData = readXYData(0x69);
+        int16_t xData = sensorData & 0xFFFF;
+        int16_t yData = (sensorData & 0xFFFF0000) >> 16;
+
+        int16_t bias = 25;
+        xData += bias;
+        yData += bias;
+        int16_t threshold = 1600;
+
         My_HAL_GPIO_WritePin(GPIOC, 6, 0);
         My_HAL_GPIO_WritePin(GPIOC, 7, 0);
         My_HAL_GPIO_WritePin(GPIOC, 8, 0);
         My_HAL_GPIO_WritePin(GPIOC, 9, 0);
-        HAL_Delay(1000);
 
-        // 6: red, 7: blue, 8: orange, 9: green
-
-        // Blip blue LED to indicate transmission is about to start
-        My_HAL_GPIO_WritePin(GPIOC, 7, 1);
-        HAL_Delay(50);
-        My_HAL_GPIO_WritePin(GPIOC, 7, 0);
-
-        // Turn on orange
-        My_HAL_GPIO_WritePin(GPIOC, 8, 1);
-
-        /* Clear the NBYTES and SADD bit fields
-         * The NBYTES field begins at bit 16, the SADD at bit 0
-         */
-        I2C2->CR2 &= ~((0x7F << 16) | (0x3FF << 0));
-        // Set SADD bits to 0x69 and NBYTES to 1
-        I2C2->CR2 |= (1 << 16) | (0x69 << 1);
-
-        // Set the read bit low
-        I2C2->CR2 &= ~(0b01 << 10);
-
-        // Set start bit
-        I2C2->CR2 |= (0b01 << 13);
-
-        // Wait for either NACKF or TXIS
-        while (!(I2C2->ISR & 0b10010))
-            ;
-
-        // Turn off orange
-        My_HAL_GPIO_WritePin(GPIOC, 8, 0);
-
-        if (I2C2->ISR & 0b10000) // NACKF
+        if (xData > threshold)
         {
-            // Send stop bit
-            I2C2->CR2 |= (0b01 << 14);
-            My_HAL_GPIO_WritePin(GPIOC, 6, 1); // Turn on red LED for NACKF
+            My_HAL_GPIO_WritePin(GPIOC, 8, 1);
         }
-        else if (I2C2->ISR & 0b10) // TXIS
+        else if (xData < threshold * -1) {
+            My_HAL_GPIO_WritePin(GPIOC, 9, 1);
+        }
+
+        if (yData > threshold)
         {
-            My_HAL_GPIO_WritePin(GPIOC, 9, 1); // Turn on green LED for TXIS
-            //My_HAL_GPIO_WritePin(GPIOC, 8, 1); // Turn on orange LED
-
-            // Write the WHO_AM_I address into the transmit data regiester
-            I2C2->TXDR = 0x0F;
-
-            // Wait for TC bit
-            while (!(I2C2->ISR & (1 << 6)))
-                ; 
-
-            // Set SADD bits to 0x69 and NBYTES to 1
-            I2C2->CR2 &= ~((0x7F << 16) | (0x3FF << 0));
-            I2C2->CR2 |= (1 << 16) | (0x69 << 1);
-            // Set the read bit high
-            I2C2->CR2 |= (0b01 << 10);
-            // Set the start bit
-            I2C2->CR2 |= (0b01 << 13);
-
-            // Wait for either NACKF or RXNE
-            while (!(I2C2->ISR & 0b10100))
-                ;
-            
-
-            while (!(I2C2->ISR & 1 << 6))
-                ; // Wait for TC bit
-
-            My_HAL_GPIO_WritePin(GPIOC, 8, 0); // Turn off orange LED
-
-            if (I2C2->RXDR == 0xD3)
-            {
-                My_HAL_GPIO_WritePin(GPIOC, 7, 1); // Turn on blue LED if WHO_AM_I register was read
-            }
-
-            // Send stop bit
-            I2C2->CR2 |= (0b01 << 14);
+            My_HAL_GPIO_WritePin(GPIOC, 6, 1);
+        }
+        else if (yData < threshold * -1) {
+            My_HAL_GPIO_WritePin(GPIOC, 7, 1);
         }
     }
+
+    
 }
